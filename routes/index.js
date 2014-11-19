7c8369ba479d080fa59a4c28b6a091b335aeef7d
ilix <code@ilix.se>
2014-11-19 20:52:58 +0100

    Added simple output of the latest songs from history.

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
 
+var history = require('../lib/history.js');
+
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+  history.all(function (err, rows) {
+    res.render('index', { title: 'Maestroo', latest: rows });
+  });
 });
 
 module.exports = router;
