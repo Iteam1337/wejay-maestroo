b0a24ed375160d7af7db215fcb3ca9900c5c7d15
irony <cl@iteam.se>
2014-11-19 22:48:44 +0100

    working tests

--- tests/integration/suggestion.js
+++ tests/integration/suggestion.js
@@ -1,3 +1,4 @@
+var moment = require('moment');
 var chai = require('chai');
 var expect = chai.expect;
 var suggestion = require(process.cwd() + '/lib/suggestion');
@@ -6,16 +7,19 @@ describe('suggestion', function () {
   var users;
 
   beforeEach(function () {
-    users = [94, 13, 88, 84];
+    users = [88];
   });
   
   describe('next', function () {
     it('returns the next song', function(done){
 
+      var startDate = moment('2011-01-01').subtract(3, 'months').valueOf();
+      var stopDate = moment().subtract(7, 'days').valueOf();
+      
       this.timeout(5000);
 
       var userId = users[Math.floor(Math.random()*4)];
-      suggestion.next(userId, function(err, song){
+      suggestion.next(userId, startDate, stopDate, function(err, song){
         expect(err).not.to.exist;
         expect(song).to.exist;
         expect(song.UserId).to.eql(userId);
