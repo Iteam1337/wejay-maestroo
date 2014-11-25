8270fbdf0a94e35b830aba8fa49717fb52ef343a
irony <cl@iteam.se>
2014-11-25 14:19:38 +0100

    error handling

--- routes/users.js
+++ routes/users.js
@@ -3,10 +3,14 @@ var db = require('../lib/db');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/import', function(req, res) {
+router.get('/import', function(req, res, next) {
   if (req.query.sure) {
-    db.setup(function(){
-      res.send('Database cleaned and imported from scratch.');
+    db.setup(function(err){
+      if (err){
+        return next(err);
+      } else {
+        res.send('Database cleaned and imported from scratch.');
+      }
     })
   } else {
     res.send('Please add ?sure=true to clean and import database from scratch')
