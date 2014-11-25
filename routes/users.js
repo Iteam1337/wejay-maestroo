cd4139b9efda6729e140e87c3d2b686099b957ab
irony <cl@iteam.se>
2014-11-25 14:09:49 +0100

    clean and setup database with a route

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,16 @@
 var express = require('express');
+var db = require('../lib/db');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res) {
-  res.send('respond with a resource');
+router.get('/import', function(req, res) {
+  if (req.query.sure) {
+    db.setup(function(){
+      res.send('Database cleaned and imported from scratch.');
+    })
+  } else {
+    res.send('Please add ?sure=true to clean and import database from scratch')
+  }
 });
 
 module.exports = router;
