64b0478ec97815042267e913531e3493e4668505
irony <cl@iteam.se>
2014-11-25 13:57:16 +0100

    use database env options

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var users = require('./routes/users');
 var connect = require('./routes/connect');
 
 var app = express();
-app.locals.moment = require('moment');
+app.locals.moment = require('moment'); // used in jade templates
 
 // view engine setup
 app.set('view engine', 'jade');
