78f54670f7bd5fbcfbadd300e096b4ed3f125a5a
Christian Landgren <cl@iteam.se>
2015-10-19 22:31:48 +0200

    Upgraded socket.io-client

--- Dockerfile
+++ Dockerfile
@@ -6,5 +6,6 @@ RUN npm install --production
 RUN ./node_modules/.bin/bower install --allow-root
 COPY . /app/
 ENV PORT=80
+ENV apiUrl=http://api.wejay.org
 EXPOSE 80
 CMD npm start 
--- routes/connect.js
+++ routes/connect.js
@@ -1,17 +1,17 @@
-var express = require('express');
-var moment = require('moment');
-var router = express.Router();
-var Room = require('../lib/room');
+var express = require('express')
+var moment = require('moment')
+var router = express.Router()
+var Room = require('../lib/room')
 
 /* GET users listing. */
-router.get('/', function(req, res) {
-  console.log('connect');
-  var room = Room.connect(req.query.room, function(){
-    room.activeUsers = room.users.filter(function(user){
-      moment(user.lastPlayDate) > moment().subtract(1, 'hour');
-    });
-    res.render('room', { room: room, title: room.roomName });
-  });
-});
+router.get('/', function (req, res) {
+  console.log('connect', req.query.room)
+  var room = Room.connect(req.query.room, function () {
+    room.activeUsers = room.users.filter(function (user) {
+      moment(user.lastPlayDate) > moment().subtract(1, 'hour')
+    })
+    res.render('room', { room: room, title: room.roomName })
+  })
+})
 
-module.exports = router;
+module.exports = router
